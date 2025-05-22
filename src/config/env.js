1646163cbd70d0ac1c1cce6ea89bb46014371f03
sam0rr <Samor140@hotmail.com>
2025-05-22 13:42:34 -0400

    feat : use env

--- src/config/env.js
+++ src/config/env.js
@@ -11,7 +11,7 @@ const config = {
     appName: process.env.APP_NAME || 'ExpressStarter',
     apiPrefix: process.env.API_PREFIX || '/api',
     connectionString: process.env.CONNECTION_STRING
-        || `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mongo:27017/${process.env.DB_NAME}?authSource=admin`
+        || `mongodb://${dbUser}:${dbPassword}@mongo:27017/${dbName}?authSource=admin`
 };
 
 module.exports = config;
